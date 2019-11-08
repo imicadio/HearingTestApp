033512897ba268808aa83ef8dacbd304f8a4acad
Michał <mrjesio@gmail.com>
2019-11-08 20:43:47 +0100

    Home HTML/CSS upgrade

--- front-hearing-app/src/containers/Home/Home.js
+++ front-hearing-app/src/containers/Home/Home.js
@@ -1,14 +1,45 @@
 import React from 'react';
 import classes from './Home.css'
-import img from "./../../assets/images/hearing_loss.png";
+// import img from "./../../assets/images/hearing_loss.png";
 
 const Home = () => {
     return(
         <React.Fragment>
-            <div className={classes.top_belt}>
-                <h2>5 minutowy test słuchu</h2>
-            </div>             
-            <div className={classes.Container}>
+            <div>
+                <div className={classes.top_belt}>
+                    <h2>5 minutowy test słuchu</h2>
+                </div>   
+                <div className={classes.content}>
+                    <div className={classes.content_inner}>
+                            <h2>Czy uważasz, że masz problem ze słuchem?</h2>
+                        <div className={classes.max_width}>
+                            <ul className={classes.answers}>
+                                <li>
+                                    <input type="radio" id="radio1" name="problem" value="Tak" />
+                                    <label for="radio1">Tak</label>
+                                </li>
+                                <li>
+                                    <input type="radio" id="radio2" name="problem" value="Nie wiem" />
+                                    <label for="radio2">Nie wiem</label>
+                                </li>
+                                <li>
+                                    <input type="radio" id="radio3" name="problem" value="Nie" />
+                                    <label for="radio3">Nie</label>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div> 
+                <div className={classes.footer}>
+                    <div className={classes.footer_inner}>
+                        <span className={classes.button}>
+                            <button>Sprawdźmy</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+
+            {/* <div className={classes.Container}>
                 <h2>Czy uważasz, że masz problem ze słuchem?</h2>                
             </div>   
             <div className={classes.Container}>
@@ -31,7 +62,7 @@ const Home = () => {
                         </li>
                     </ul>
                 </form>
-            </div>
+            </div> */}
         </React.Fragment>
     );
 }
