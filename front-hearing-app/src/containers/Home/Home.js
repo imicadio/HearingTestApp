5973a2336d3f2d146573e516acfdbca3e25099d9
Michał <mrjesio@gmail.com>
2019-11-09 00:23:17 +0100

    Answers HTML/CSS v1.0

--- front-hearing-app/src/containers/Home/Home.js
+++ front-hearing-app/src/containers/Home/Home.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import Footer from './../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import Content from '../../components/Content/Content';
+import Answers from '../../components/Answers/Answers';
 
 import './Home.css'
 import img from "./../../assets/images/hearing-01.svg";
-import Content from '../../components/Content/Content';
-import Answers from '../../components/Answers/Answers';
 
 const Home = () => {
     
--- front-hearing-app/src/containers/Setup-Instruction/Setup-Instruction.js
+++ front-hearing-app/src/containers/Setup-Instruction/Setup-Instruction.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import Footer from './../../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Content from '../../components/Content/Content';
+
 const SetupInstruction = () => {
+    const headerText = "Przed rozpoczęciem";
+    const link = "";
+
     return(
         <React.Fragment>
-            <div><h1>Test SetupInstruction</h1></div>
+            <div>
+                <Header text={headerText} />  
+                <Content>
+                    <h2>Upewnij się, że przebywasz w cichym pomieszczeniu.</h2>
+                </Content>
+                <Footer link={link}/>
+            </div>
         </React.Fragment>
     );
 }
