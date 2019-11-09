8b283c075c7e0fda8ae223462b3dd01f3c56d94d
Michał <mrjesio@gmail.com>
2019-11-09 02:10:31 +0100

    Footer upgrade

--- front-hearing-app/src/components/Footer/Footer.js
+++ front-hearing-app/src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import { NavLink } from 'react-router-dom';
 import classes from './Footer.css';
 
 const Footer = props => {
-    return(
+
+    let navigation = (
         <div className={classes.footer}>
             <div className={classes.footer_inner}>
                 <span className={classes.button}>
@@ -13,6 +14,31 @@ const Footer = props => {
                 </span>
             </div>
         </div>
+    );
+
+    if(props.link !== "/setupInstructions"){
+        navigation = (
+            <div className={classes.footer}>
+                <div className={classes.footer_inner}>
+                    <NavLink to="/">
+                        <span className={classes.button_back}>
+                            <i className={classes.left}></i>                            
+                        </span>
+                    </NavLink>
+                    <span className={classes.button}>
+                        <NavLink to={props.link}>
+                            <button>Dalej</button>
+                        </NavLink>                            
+                    </span>
+                </div>
+            </div>            
+        );
+    }
+
+    return(
+        <React.Fragment>
+            {navigation}
+        </React.Fragment>
     );
 }
 
