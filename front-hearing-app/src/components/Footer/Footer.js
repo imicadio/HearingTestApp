27ff4d7f571869f23beffbdad2c59543d9ec6f9f
Michal Jeszko <michal.jeszko@sprint.pl>
2020-09-05 11:17:46 +0200

    HomePage button

--- front-hearing-app/src/components/Footer/Footer.js
+++ front-hearing-app/src/components/Footer/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import classes from './Footer.css';
+import Icon from '@material-ui/core/Icon';
 
 const Footer = props => {
 
     let btnFooter = (
-        <button className={classes.btnFooter}>Do dzieła!<span className={classes.iconNext}></span></button>
+        <button className={classes.btnFooter}>Do dzieła!<span className={classes.footer__icon__next}><Icon>keyboard_arrow_right</Icon></span></button>
     );
 
     return(
